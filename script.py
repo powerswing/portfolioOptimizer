1ad6346dadb2d1cb347316937840d43c41ac13da
swordswing <eleven@gscw.ru>
2021-05-10 21:21:26 +0200

    fifth commit

--- script.py
+++ script.py
@@ -25,15 +25,15 @@ startDate = endDate - dt.timedelta(days=history)
 
 def getData(stocks, startDate, endDate=dt.datetime.now()):
     """
-    Parces stocks over given period from Yahoo. If end date (endDate) is not passed, then it is taken to be today
+    Parces stocks over given period from Yahoo
 
     Parameters
         stocks : list
             list of stocks to be parced
         startDate : datetime.datetime
-            start day of desired period
-        endDate : datetime.datetime
-            end day of desired period
+            start date of desired period
+        endDate : datetime.datetime, default is today
+            end date of desired period
 
     Returns
         data : pd.DataFrame
@@ -50,16 +50,12 @@ class portfolioOptimizer(object):
     # TODO: Add description
     """
 
-    def __init__(self, data, capital, assertNormality=True):
+    def __init__(self, data, capital, simulationNumber=10):
         self.data = data
         self.capital = capital
-        self.alpha = 0.05
-        self.simulationNumber = 10
-        self.timeframe = 1
-        self.percentile = 5
-        self.riskfreeRate = 0
-        self.assertNormality = assertNormality
+        self.simulationNumber = simulationNumber
         self.stocks = None
+        self.stocksNumber = None
         self.returnsRealizations = None
         self.closingPrices = None
         self.pctReturns = None
@@ -70,11 +66,12 @@ class portfolioOptimizer(object):
         self.kurtosis = None
         self.normalityMask = None
         self.optimalWeights = None
-        self.weights = np.zeros(self.simulationNumber)
-        self.weightedReturns = np.zeros(self.simulationNumber)
-        self.weightedVariances = np.zeros(self.simulationNumber)
-        self.weightedRisks = np.zeros(self.simulationNumber)
-        self.sharpeRatios = np.zeros(self.simulationNumber)
+        self.weights = None
+        self.weightedReturns = None
+        self.weightedVariances = None
+        self.weightedRisks = None
+        self.sharpeRatios = None
+        self.bestWeight = None
         self.meanReturnsRealizations = None
         self.L = None
         self.Z = None
@@ -83,17 +80,17 @@ class portfolioOptimizer(object):
         self.conditionalValueAtRisk = None
         self.returnsRealizationsLast = None
 
-    def getMoments(self):
+    def getMoments(self, assertNormality=True, alpha=0.05):
         """
         Calculates empirical mean and covariance matrix of closing prices for given data. If assertNormality is True, asserts normality of returns for given stocks and calculates kurtosis. Those stocks that fail null hypothesis of returns normality are removed. Normality test is based D’Agostino and Pearson’s (1973)
 
         Parameters
-            data : pd.DataFrame
-                raw parsed data from yahoo with "Close" column to extract closing prices. To parse raw data from Yahoo, get_data_yahoo function of pandas_datareader.data must be used
             assertNormality : bool
                 whether to trim data of stocks which returns fail normality test
+            alpha : float
+                critical value of chi-squared distribution
 
-        Returns
+        Stores
             closingPrices : pd.DataFrame
                 closing prices of parsed data (NaNs are dropped)
             pctReturns : pd.Series
@@ -105,18 +102,20 @@ class portfolioOptimizer(object):
         """
         self.closingPrices = self.data["Close"]
         self.stocks = self.closingPrices.columns.values
+        self.stocksNumber = len(self.stocks)
         # self.closingPricesM = self.closingPricesD.sort_index().resample(
         #     "M").apply(lambda x: x.iloc[-1, ])
         # self.closingPricesM = self.closingPricesD.resample("M").mean()
 
         self.pctReturns = self.closingPrices.pct_change().dropna()
 
-        if self.assertNormality:
+        if assertNormality:
             self.statistic, self.pvalue = normaltest(self.pctReturns)
             self.kurtosis = kurtosis(self.pctReturns)
-            self.normalityMask = self.pvalue < self.alpha
+            self.normalityMask = self.pvalue < alpha
             self.stocks = [stock for (stock, mask) in zip(
                 self.stocks, self.normalityMask) if mask]
+            self.stocksNumber = len(self.stocks)
 
             for i in [self.closingPrices, self.pctReturns]:
 
@@ -126,22 +125,35 @@ class portfolioOptimizer(object):
         self.meanReturns = self.pctReturns.mean()
         self.covarianceMatrix = self.pctReturns.cov()
 
-        return self.closingPrices, self.pctReturns, self.meanReturns, self.covarianceMatrix
-
-    def initiateWeights(self):
+    def simulateWeights(self, riskfreeRate=0):
         """
-        Optimizes weights for given stocks using Sharpe ratio (Sharpe 1966) under normality assumption
+        Optimizes weights for given stocks using Sharpe ratio (Sharpe 1966) under assumption of returns normality
 
         Parameters
-
-        Returns
+            riskfreeRate : float, default is 0
+                Risk-free rate of return used to calculate Sharpe ratio
+        Stores
+            #TODO
 
 
         """
+        self.weights = np.zeros(
+            [self.simulationNumber, self.stocksNumber])
+        self.weightedReturns = np.zeros(
+            [self.simulationNumber, self.stocksNumber])
+        self.weightedVariances = np.zeros(
+            [self.simulationNumber, self.stocksNumber])
+        self.weightedRisks = np.zeros(
+            [self.simulationNumber, self.stocksNumber])
+        self.sharpeRatios = np.zeros(
+            [self.simulationNumber, self.stocksNumber])
+        self.bestWeight = np.zeros(
+            [1, self.stocksNumber])
+
         for simulation in range(self.simulationNumber):
 
-            weight = np.random.random(len(self.stocks))
-            weight /= np.sum(self.weights)
+            weight = np.random.random(self.stocksNumber)
+            weight /= np.sum(weight)
             self.weights[simulation] = weight
 
             weightedReturn = np.sum(self.meanReturns * weight)
@@ -153,11 +165,15 @@ class portfolioOptimizer(object):
             self.weightedVariances[simulation] = weightedVariance
             self.weightedRisks[simulation] = weightedStandardDeviation
 
-            sharpeRatio = (weightedReturn - self.riskfreeRate) / \
+            sharpeRatio = (weightedReturn - riskfreeRate) / \
                 weightedStandardDeviation
             self.sharpeRatios[simulation] = sharpeRatio
 
-    def simulateReturns(self):
+        self.bestWeight = self.weights[np.argmax(self.sharpeRatios[:, 0])]
+
+        #self.bestWeight = self.weights[np.argmax(po.sharpeRatios)]
+
+    def simulateReturns(self, timeframe=1):
         """
         Implements Normal Carlo simulation (f.e. Raychaudhuri 2008) using Cholesky Decomposition (f.e. Highham 2009) for covariance matrix. Function returns matrix of simulated returns given by S = M + Z*L where M is matrix of empirical average relative returns, Z is multivariate normal matrix and L is lower triangular matrix of Cholesky decomposition of empirical covariance matrix, and * is the inner product operator
 
@@ -172,34 +188,32 @@ class portfolioOptimizer(object):
                 covariance matrix of relative returns for given stocks
             simulationNumber : int, default is 1000
                 number of simulations to run
-            timeframe : int, default is 9
+            timeframe : int, default is 1
                 length of time path of each realization from today
 
-        Returns
+        Stores
             returnsRealizations : np.ndarray
                 matrix of simulations of returns
 
         """
         # matrix of means
         self.meanReturnsRealizations = np.full(
-            shape=(self.timeframe, len(self.stocks)), fill_value=self.meanReturns).T
+            shape=(timeframe, self.stocksNumber), fill_value=self.meanReturns).T
 
         self.returnsRealizations = np.full(
-            shape=(self.timeframe, self.simulationNumber), fill_value=0.0)
+            shape=(timeframe, self.simulationNumber), fill_value=0.0)
         # Cholesky decomposition of covariance matrix
         self.L = np.linalg.cholesky(self.covarianceMatrix)
 
         for simulation in range(self.simulationNumber):
             self.Z = np.random.normal(
-                size=(self.timeframe, len(self.stocks)))
+                size=(timeframe, self.stocksNumber))
             self.simulationReturns = self.meanReturnsRealizations + \
                 np.inner(self.L, self.Z)
             self.returnsRealizations[:, simulation] = np.cumprod(
-                np.inner(self.weights, self.simulationReturns.T) + 1) * self.capital
+                np.inner(self.bestWeight, self.simulationReturns.T) + 1) * self.capital
 
-        return self.returnsRealizations
-
-    def riskMetrics(self):
+    def riskMetrics(self, percentile=5):
         """
         Returns the inverse of value-at-risk and conditional value-at-risk for given percentile level at the tail of simulation paths. To obtain the risk metrics, the return values are to be subtracted from capital
 
@@ -208,10 +222,10 @@ class portfolioOptimizer(object):
                 value of investment
             returnsRealizations : np.ndarray
                 matrix of simulations of returns
-            percentile : float, default : 5 (translates to 95 percentile)
+            percentile : float, default is 5 (translates to 95 percentile)
                 percentile (aka confidence or percentile) level for loss cutoff
 
-        Returns
+        Stores
             valueAtRisk : float
                 value-at-risk for given confidence level
             conditionalValueAtRisk : float
@@ -220,16 +234,18 @@ class portfolioOptimizer(object):
         self.returnsRealizationsLast = pd.Series(
             self.returnsRealizations[-1, :])
         self.valueAtRisk = np.percentile(
-            self.returnsRealizationsLast, self.percentile)
+            self.returnsRealizationsLast, percentile)
         self.conditionalValueAtRisk = self.returnsRealizationsLast[self.returnsRealizationsLast <= self.valueAtRisk].mean(
         )
 
-        return self.capital - self.valueAtRisk, self.capital - self.conditionalValueAtRisk
+        self.valueAtRisk = self.capital - self.valueAtRisk
+        self.conditionalValueAtRisk = self.capital - self.conditionalValueAtRisk
 
 
 # %%
 data = getData(stocks, startDate, endDate)
 po = portfolioOptimizer(data, capital)
-closingPrices, pctReturns, meanReturns, covarianceMatrix = po.getMoments()
-returnsRealizations = po.simulateReturns()
-var, cvar = po.riskMetrics()
+po.getMoments()
+po.simulateWeights()
+po.simulateReturns()
+po.riskMetrics()
